3686ab77c19eb15249b7b743b725373d6b243284
Bruno Cantisano <bruno.cantisano@gmail.com>
2017-08-05 20:28:54 -0300

    passing password file to be used in environment variable

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-docker run -d -p 3690:3690 -p 8083:80 -p 443:443 -v /media/usbraid/svn:/var/svn -v /media/usbraid/docker/svn/log:/var/log/apache2 --name svnRepo paperinik/rpi-svn
+docker run -d -p 3690:3690 \
+-p 8083:80 -p 443:443 \
+-v /media/usbraid/svn:/var/svn \
+-v /media/usbraid/docker/svn/log:/var/log/apache2 \
+--env PASSWD_FILE=/media/usbraid/docker/svn/dav_svn.passwd
+--name svnRepo paperinik/rpi-svn
