3d85dba51052dc87f16283508e8b2a4bd4ea200e
brunocantisano <bruno.cantisano@gmail.com>
2017-08-05 20:31:29 -0300

    changed parameter

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ docker run -d -p 3690:3690 \
 -p 8083:80 -p 443:443 \
 -v /media/usbraid/svn:/var/svn \
 -v /media/usbraid/docker/svn/log:/var/log/apache2 \
---env PASSWD_FILE=/media/usbraid/docker/svn/dav_svn.passwd
+-e PASSWD_FILE=/media/usbraid/docker/svn/dav_svn.passwd
 --name svnRepo paperinik/rpi-svn
