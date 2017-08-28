41ed5772d8daa6cc279ff7f28b12f1585ef13478
brunocantisano <bruno.cantisano@gmail.com>
2017-08-27 22:23:12 -0300

    changing port and removing unnecessary files

--- files/run-param.sh
+++ files/run-param.sh
@@ -2,7 +2,7 @@
 #
 if [ ! "$1" = "" ] && [ ! "$2" = "" ] && [ ! "$3" = "" ]; then
     echo -e "\ncreating container $1\n\n"
-    docker run -d -p 3690:3690 -p 9401:80 -p 443:443 -v $2:/var/svn -v $3:/var/log/apache2 --name $1 paperinik/rpi-svn
+    docker run -d -p 3690:3690 -p 9403:80 -p 443:443 -v $2:/var/svn -v $3:/var/log/apache2 --name $1 paperinik/rpi-svn
 else
     echo -e "\nUSAGE: run.sh your-container-name your-path security-path\n\n"
 fi
